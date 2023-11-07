3282b2e38fc1ebd5e977550bd2eb8d5d3d41e658
wiersinga <wiersingaolfa@yahoo.fr>
2023-11-07 16:57:38 +0100

    create tests and their functions

---
+++ toDoList.js
@@ -0,0 +1,68 @@
+
+export function addNewList(arraylists, newList){
+    arraylists.push(newList)
+}
+
+export function addNewTask(list, newTask){
+    list.tasks.push(newTask)
+}
+
+export function markOneTaskAsDone(list, indexOfTaskToMarkAsDone){
+    if (list.tasks[indexOfTaskToMarkAsDone] !== undefined){
+        if (list.tasks[indexOfTaskToMarkAsDone].done === false) {
+            list.tasks[indexOfTaskToMarkAsDone].done = true;
+        }
+    }
+}
+
+export function markOneTaskAsUndone(list,indexOfTaskToMarkAsUndone){
+    if (list.tasks[indexOfTaskToMarkAsUndone] !== null){
+        if (list.tasks[indexOfTaskToMarkAsUndone].done === true){
+            list.tasks[indexOfTaskToMarkAsUndone].done = false
+        }
+    }
+}
+
+export function markAllAsDone(list){
+    if (list.tasks.length > 0){
+        list.tasks.forEach(task =>{
+            if (task.done === false){
+                task.done = true
+            }
+        })
+    }
+}
+
+export function markAllAsUndone(list){
+    if (list.tasks.length > 0){
+        list.tasks.forEach(task =>{
+            if (task.done === true){
+                task.done = false
+            }
+        })
+    }
+}
+
+export function deleteAList(arrayList, indexOfListToDelete){
+    if (arrayList[indexOfListToDelete] !== null){
+       delete arrayList[indexOfListToDelete]
+    }
+}
+
+export function deleteAllLists(arrayList){
+    if (arrayList.length > 0){
+        arrayList.splice(0, arrayList.length)
+    }
+}
+
+export function deleteATask(arrayList, indexOfList, indexOfTaskToDelete){
+    if (arrayList[indexOfList].tasks[indexOfTaskToDelete] !== undefined){
+        delete arrayList[indexOfList].tasks[indexOfTaskToDelete]
+    }
+}
+
+export function deleteAllTasks(arrayList, indexOfList){
+    if (arrayList[indexOfList].tasks.length > 0){
+        arrayList[indexOfList].tasks.length = 0
+    }
+}
--- toDoList.test.js
+++ toDoList.test.js
@@ -1,33 +1,35 @@
 import {expect, test} from "vitest"
-import {addNewList} from './addNewList'
+import {
+    addNewList,
+    addNewTask,
+    deleteAList,
+    markOneTaskAsDone,
+    markOneTaskAsUndone,
+    markAllAsDone,
+    markAllAsUndone,
+    deleteAllLists,
+    deleteAllTasks,
+    deleteATask
+} from './toDoList'
+
+//test 1 : add new list
 test('addNewList', () => {
-    var arrayList = []
-    var list = {
+    const arrayLists = []
+    let newList = {
         "name":'nameList',
-        "tasks": [
-            {"label":"",
-             "done":""}
-        ]
-    }
-    var list2 = {
-        "name": "",
-        "tasks": [
+        tasks: [
             {"label":"",
              "done":""}
         ]
     }
 
-    arrayList.push(list)
-    var newListName = "newList2"
-
-    console.log(arrayList);
-    const modifiedListlength = addNewList(arrayList, newListName).length;
-    expect(modifiedListlength).toBe(2);
+    addNewList(arrayLists, newList);
+    expect(arrayLists).toContain(newList);
 })
-
+//test 2 : add new task
 test('add new Task', ()=> {
 
-    var list = {
+    let list = {
         "name": 'nameList',
         "tasks": [
             {
@@ -37,29 +39,256 @@ test('add new Task', ()=> {
         ]
     }
 
-    var newTask = {
+    let newTask = {
         "label" : "changer les draps",
          "done" : false
     }
 
-    list.tasks.push(newTask)
-    console.log(list)
-
+    addNewTask(list, newTask);
     expect(list.tasks).toContain(newTask);
 })
 
-test('markAsDone', ()=> {
-     var list= {
-         "name" : "home list"  ,
-         "tasks": [{
-             "label": "fermer la porte",
-             "done": false
-         }]
-     }
-     var newDone = true
-     if (list.tasks.done == false){
+//test 3 : mark one task as Done
+test('markAsDone: defined', () => {
+    let list = {
+        "name": "home list",
+        tasks: [{
+            "label": "fermer la porte",
+            "done": true
+        },
+        {
+                "label": "nettoyer la cuisine",
+                "done": false
+        }]
+    }
 
-   
-     }
+    const indexOfTaskToMarkAsDone = 0;
+
+
+
+    markOneTaskAsDone(list, indexOfTaskToMarkAsDone);
+        expect(list.tasks[indexOfTaskToMarkAsDone].done).toEqual(true)
 
 })
+test('markAsDone: undefined', () => {
+    let list = {
+        "name": "home list",
+        tasks: [{
+            "label": "fermer la porte",
+            "done": false
+        },
+            {
+                "label": "nettoyer la cuisine",
+                "done": false
+            }]
+    }
+
+    const indexOfTaskToMarkAsDone = 6;
+
+    markOneTaskAsDone(list, indexOfTaskToMarkAsDone);
+    expect(list.tasks[indexOfTaskToMarkAsDone]).toBeUndefined()
+})
+
+//test 4 : mark one task as undone
+
+test('mark as undone', () => {
+    let list = {
+        "name": "home list",
+        tasks: [{
+            "label": "fermer la porte",
+            "done": true
+        },
+            {
+                "label": "nettoyer la cuisine",
+                "done": true
+            }]
+    }
+    const indexOfTaskToMarkAsUndone = 1;
+
+    markOneTaskAsUndone(list,indexOfTaskToMarkAsUndone);
+    expect(list.tasks[indexOfTaskToMarkAsUndone].done).toEqual(false);
+
+})
+
+//test 5 : mark all tasks as done
+
+test('mark all as done', () => {
+    let list = {
+        "name": "home list",
+        tasks: [{
+            "label": "fermer la porte",
+            "done": false
+        },
+        {
+                "label": "nettoyer la cuisine",
+                "done": false
+        }]
+    }
+    // list.tasks.forEach(task => {
+    //     if (task.done === false){
+    //         task.done = true
+    //     }
+    // })
+    // console.log(list)
+
+    markAllAsDone(list)
+    expect(list.tasks.every(task => task.done)).toBe(true)
+
+    // const allTaskDone = list.tasks.every(task => task.done === true)
+    // expect(allTaskDone).toBe(true)
+
+})
+
+//test 6 : mark all tasks as undone
+
+test('mark all tasks as undone', () => {
+    let list = {
+        "name": "home list",
+        tasks: [{
+            "label": "fermer la porte",
+            "done": true
+        },
+        {
+                "label": "nettoyer la cuisine",
+                "done": true
+        }]
+    }
+
+    markAllAsUndone(list)
+
+    const allTasksUndone = list.tasks.every(task => task.done === false)
+    expect(allTasksUndone).toEqual(true)
+})
+
+//test 7 : delete a list
+
+test('delete a list', () => {
+    let arrayList = [
+        {
+            "name": "home list",
+            tasks: [{
+                "label": "fermer la porte",
+                "done": true
+            },
+            {
+                    "label": "nettoyer la cuisine",
+                    "done": true
+            }]
+        },
+        {
+            "name": "car list",
+            tasks: [{
+                "label": "fermer la porte",
+                "done": true
+            },
+            {
+                    "label": "nettoyer la cuisine",
+                    "done": true
+            }]
+        }
+    ]
+
+    const indexOfListToDelete = 0;
+
+    deleteAList(arrayList, indexOfListToDelete)
+    expect(arrayList).not.toContain(arrayList[indexOfListToDelete]);
+})
+
+//test 8 : delete all the lists
+test('delete all lists',() => {
+
+    let arrayList = [
+        {
+            "name": "home list",
+            tasks: [{
+                "label": "fermer la porte",
+                "done": true
+            },
+            {
+                    "label": "nettoyer la cuisine",
+                    "done": true
+            }]
+        },
+        {
+            "name": "car list",
+            tasks: [{
+                "label": "fermer la porte",
+                "done": true
+            },
+                {
+                    "label": "nettoyer la cuisine",
+                    "done": true
+                }]
+        }
+    ]
+    deleteAllLists(arrayList)
+    expect(arrayList.length).toEqual(0)
+})
+
+// test 9: delete a task
+
+test('delete a task', () => {
+    let arrayList = [
+        {
+            "name": "home list",
+            tasks: [{
+                "label": "fermer la porte",
+                "done": true
+            },
+                {
+                    "label": "nettoyer la cuisine",
+                    "done": true
+                }]
+        },
+        {
+            "name": "car list",
+            tasks: [{
+                "label": "fermer la porte",
+                "done": true
+            },
+                {
+                    "label": "nettoyer la cuisine",
+                    "done": true
+                }]
+        }
+    ]
+    const indexOfList = 0
+    const indexOfTaskToDelete = 1
+    deleteATask(arrayList, indexOfList, indexOfTaskToDelete)
+    expect(arrayList[indexOfList].tasks[indexOfTaskToDelete]).toBeUndefined()
+})
+
+// test 10: delete all tasks
+test('deleteAllTasks', () => {
+    let arrayList = [
+        {
+            "name": "home list",
+            tasks: [{
+                "label": "fermer la porte",
+                "done": true
+            },
+                {
+                    "label": "nettoyer la cuisine",
+                    "done": true
+                }]
+        },
+        {
+            "name": "car list",
+            tasks: [{
+                "label": "fermer la porte",
+                "done": true
+            },
+                {
+                    "label": "nettoyer la cuisine",
+                    "done": true
+                }]
+        }
+    ]
+
+    const indexOfList = 1;
+    deleteAllTasks(arrayList, indexOfList)
+    expect(arrayList[indexOfList].tasks.length).toEqual(0)
+})
+
+
+
